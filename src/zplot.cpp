b30a49db43303b61c5c333e60b67d28536e221bc
ssteinbach <ssteinbach@users.noreply.github.com>
2025-11-29 10:49:37 -0800

    Add stairstep binding/demo

--- src/zplot.cpp
+++ src/zplot.cpp
@@ -226,6 +226,67 @@ extern "C"
             assert(false);
     }
 
+    ZGUI_API void zguiPlot_PlotStairsValues(
+        const char *label_id,
+        ImGuiDataType data_type,
+        const void *values,
+        int count,
+        double xscale,
+        double x0,
+        ImPlotStairsFlags flags,
+        int offset,
+        int stride)
+    {
+        if (data_type == ImGuiDataType_S8)
+            ImPlot::PlotStairs(label_id, (const ImS8 *)values, count, xscale, x0, flags, offset, stride);
+        else if (data_type == ImGuiDataType_U8)
+            ImPlot::PlotStairs(label_id, (const ImU8 *)values, count, xscale, x0, flags, offset, stride);
+        else if (data_type == ImGuiDataType_S16)
+            ImPlot::PlotStairs(label_id, (const ImS16 *)values, count, xscale, x0, flags, offset, stride);
+        else if (data_type == ImGuiDataType_U16)
+            ImPlot::PlotStairs(label_id, (const ImU16 *)values, count, xscale, x0, flags, offset, stride);
+        else if (data_type == ImGuiDataType_S32)
+            ImPlot::PlotStairs(label_id, (const ImS32 *)values, count, xscale, x0, flags, offset, stride);
+        else if (data_type == ImGuiDataType_U32)
+            ImPlot::PlotStairs(label_id, (const ImU32 *)values, count, xscale, x0, flags, offset, stride);
+        else if (data_type == ImGuiDataType_Float)
+            ImPlot::PlotStairs(label_id, (const float *)values, count, xscale, x0, flags, offset, stride);
+        else if (data_type == ImGuiDataType_Double)
+            ImPlot::PlotStairs(label_id, (const double *)values, count, xscale, x0, flags, offset, stride);
+        else
+            assert(false);
+    }
+
+    ZGUI_API void zguiPlot_PlotStairs(
+        const char *label_id,
+        ImGuiDataType data_type,
+        const void *xv,
+        const void *yv,
+        int count,
+        ImPlotStairsFlags flags,
+        int offset,
+        int stride)
+    {
+        if (data_type == ImGuiDataType_S8)
+            ImPlot::PlotStairs(label_id, (const ImS8 *)xv, (const ImS8 *)yv, count, flags, offset, stride);
+        else if (data_type == ImGuiDataType_U8)
+            ImPlot::PlotStairs(label_id, (const ImU8 *)xv, (const ImU8 *)yv, count, flags, offset, stride);
+        else if (data_type == ImGuiDataType_S16)
+            ImPlot::PlotStairs(label_id, (const ImS16 *)xv, (const ImS16 *)yv, count, flags, offset, stride);
+        else if (data_type == ImGuiDataType_U16)
+            ImPlot::PlotStairs(label_id, (const ImU16 *)xv, (const ImU16 *)yv, count, flags, offset, stride);
+        else if (data_type == ImGuiDataType_S32)
+            ImPlot::PlotStairs(label_id, (const ImS32 *)xv, (const ImS32 *)yv, count, flags, offset, stride);
+        else if (data_type == ImGuiDataType_U32)
+            ImPlot::PlotStairs(label_id, (const ImU32 *)xv, (const ImU32 *)yv, count, flags, offset, stride);
+        else if (data_type == ImGuiDataType_Float)
+            ImPlot::PlotStairs(label_id, (const float *)xv, (const float *)yv, count, flags, offset, stride);
+        else if (data_type == ImGuiDataType_Double)
+            ImPlot::PlotStairs(label_id, (const double *)xv, (const double *)yv, count, flags, offset, stride);
+        else
+            assert(false);
+    }
+
     ZGUI_API void zguiPlot_PlotShaded(
         const char *label_id,
         ImGuiDataType data_type,
