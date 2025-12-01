541aa59439536969ed181431569ece8712c88b59
ssteinbach <ssteinbach@users.noreply.github.com>
2025-12-01 09:50:52 -0800

    add example from zgui on canvas drawing

--- src/app_wrapper_demo.zig
+++ src/app_wrapper_demo.zig
@@ -676,6 +676,115 @@ fn draw(
                     .{ .x = wsize[0], .y = wsize[1]},
                 );
             }
+
+            if (
+                zgui.beginTabItem("Canvas Drawing Example", .{})
+                and zgui.beginChild(
+                    "GraphView",
+                    .{
+                        .h = -1,
+                        .w = -1,
+                        .child_flags = .{},
+                        .window_flags = .{
+                            .menu_bar = false,
+                        }
+                    },
+                )
+            )
+            {
+                defer zgui.endTabItem();
+                defer zgui.endChild();
+
+                zgui.beginGroup();
+
+                zgui.textUnformatted("hi");
+
+                const dl = zgui.getWindowDrawList();
+
+                dl.addQuad(
+                    .{
+                        .p1 = .{ 170, 420 },
+                        .p2 = .{ 270, 420 },
+                        .p3 = .{ 220, 520 },
+                        .p4 = .{ 120, 520 },
+                        .col = 0xff_00_00_ff,
+                        .thickness = 3.0,
+                    }
+                );
+                dl.addText(
+                    .{ 130, 130 },
+                    0xff_00_00_ff,
+                    "The number is: {}",
+                    .{7}
+                );
+                dl.addCircleFilled(
+                    .{
+                        .p = .{ 200, 600 },
+                        .r = 50,
+                        .col = 0xff_ff_ff_ff 
+                    },
+                );
+                dl.addCircle(
+                    .{
+                        .p = .{ 200, 600 },
+                        .r = 30,
+                        .col = 0xff_00_00_ff,
+                        .thickness = 11,
+                    }
+                );
+                dl.addPolyline(
+                    &.{
+                        .{ 100, 700 },
+                        .{ 200, 600 },
+                        .{ 300, 700 },
+                        .{ 400, 600 },
+                    },
+                    .{
+                        .col = 0xff_00_aa_11,
+                        .thickness = 7,
+                    },
+                );
+
+
+                const c1 = zgui.colorConvertFloat4ToU32(.{0.8, 0.2, 0.2, 0.4});
+                const c2 = zgui.colorConvertFloat4ToU32(.{0.2, 0.8, 0.2, 0.4});
+                const c3 = zgui.colorConvertFloat4ToU32(.{0.2, 0.2, 0.8, 0.4});
+                const c4 = zgui.colorConvertFloat4ToU32(.{0.8, 0.8, 0.8, 0.9});
+
+                dl.addRect(
+                    .{
+                        // .col = 0xff_bb_bb_ff,
+                        .col = c4,
+                        .pmin = .{ 200, 200 },
+                        .pmax = .{ 300, 300 },
+                        .rounding = 0.4,
+                    },
+                );
+
+                dl.addCircleFilled(
+                    .{ 
+                        .col = c1,
+                        .p = .{ 100, 100 }, 
+                        .r = 60,
+                    },
+                );
+                dl.addCircleFilled(
+                    .{
+                        .col = c2,
+                        .p = .{ 300, 312 },
+                        .r = 30,
+                    },
+                );
+                dl.addCircleFilled(
+                    .{
+                        .col = c3,
+                        .p = .{ 120, 120 },
+                        .r = 60,
+                    },
+                );
+
+                zgui.endGroup();
+            }
         }
     }
 }
